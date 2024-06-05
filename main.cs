91c9e43a2eab5577c33c423a3c2ec8b356b2cc03
Matt Ramey <110432461+mattramey@users.noreply.github.com>
2024-06-05 21:59:01 +0000

    Second Commit

--- main.cs
+++ main.cs
@@ -2,17 +2,22 @@ using System;
 
 class Twitter { //open class Twitter
   public static void Main (string[] args) {// open Main
+//Loop through the program 4 times to prompt the user 4 times.
     for(int i = 1; i <=4; i++){// open for loop
     Console.WriteLine ("Please enter what you want to Tweet. (140 characters max");
       Console.WriteLine();
+// declare a variable to store the user's input.
     string tweet = Console.ReadLine();
 
+  // creat an if-else to chech if the number of characters the user typed is less than or equal to 140. if <= computer will print "Posted" if >= the compter will print "Rejected"
     if(tweet.Length <=140){//open if
+// print a space for better readability
       Console.WriteLine();
       Console.WriteLine("Posted\n");
       Console.WriteLine("Test Passed\n");
     }// end if
       else{//open else
+// print a space for better readability
         Console.WriteLine();
         Console.WriteLine("Rejected\n");
         Console.WriteLine("Test Passed\n");
